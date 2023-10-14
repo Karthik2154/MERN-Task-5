0dc3aa21166094b4f2ec7e135655ab039c0a299c
Karthik2154 <karthik.v2021@vitstudent.ac.in>
2023-10-14 21:24:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Task5 from "./Components/Task5";
+import "./bootstrap-5.0.2-dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <Task5 />
+    </div>
+  );
+}
+
+export default App;
